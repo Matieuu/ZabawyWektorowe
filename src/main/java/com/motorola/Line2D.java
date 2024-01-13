0882e0b7390ee83a0054884bb1641c215de49dcc
ArtMan0 <artur.2006@o2.pl>
2024-01-13 18:59:12 +0100

    Line2D

---
+++ src/main/java/com/motorola/Line2D.java
@@ -0,0 +1,15 @@
+package com.motorola;
+import java.awt.*;
+
+
+public class Line2D {
+    public Vector2 start;
+    public Vector2 end;
+    public Color color;
+
+    public Line2D(Vector2 start_, Vector2 end_, Color color_){
+        start = start_;
+        end = end_;
+        color = color_;
+    }
+}
