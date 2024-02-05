7d7b09b3c4c6c16b179199d2494d2cbeb989cd80
ArtMan0 <artur.2006@o2.pl>
2024-02-05 13:33:27 +0100

    Change plan

--- src/main/java/com/motorola/tempest/TempestState.java
+++ src/main/java/com/motorola/tempest/TempestState.java
@@ -1,13 +1,21 @@
 package com.motorola.tempest;
 
 import com.motorola.engine.Game;
+import com.motorola.engine.GameObject;
+import com.motorola.engine.GameSystem;
 import com.motorola.engine.default_systems.Renderer3D;
+import com.motorola.engine.graphics.Model;
+import com.motorola.engine.graphics.Quaternion;
+import com.motorola.engine.graphics.Transform;
+import com.motorola.engine.graphics.Vector3;
 import com.motorola.engine.states.State;
 
 public class TempestState extends State {
 
     private Renderer3D render3d;
-    private LevelControlerSystem levelControlerSystem;
+    private Model firstLevel;
+    public GameObject levelObject;
+    public GameObject player;
 
     public TempestState(Game game) {
         super(game);
@@ -16,7 +24,16 @@ public class TempestState extends State {
     @Override
     public State load() {
         render3d = new Renderer3D(game);
-        levelControlerSystem = new LevelControlerSystem(game,render3d);
+
+        levelObject = new GameObject(game,"Level");
+        levelObject.setValue("Model",firstLevel);
+        levelObject.setValue("Transform", new Transform(new Vector3(0,0,0), Quaternion.createEulerAngles(0,0,0)));
+
+        render3d.addObject(levelObject);
+        player = new GameObject(game,"Player");
+        levelObject.setValue("Model", firstLevel);
+        levelObject.setValue("Transform", new Transform(new Vector3(0,0,0), Quaternion.createEulerAngles(0,0,0)));
+        render3d.addObject(player);
 
         return this;
     }
