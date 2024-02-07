1bdb4a7fce03d35dffb0d4c0da133b2e6a945db7
ArtMan0 <artur.2006@o2.pl>
2024-02-07 23:39:41 +0100

    )':

--- src/main/java/com/motorola/tempest/TempestState.java
+++ src/main/java/com/motorola/tempest/TempestState.java
@@ -4,19 +4,21 @@ import com.motorola.engine.Game;
 import com.motorola.engine.GameObject;
 import com.motorola.engine.GameSystem;
 import com.motorola.engine.default_systems.Renderer3D;
-import com.motorola.engine.graphics.Model;
-import com.motorola.engine.graphics.Quaternion;
-import com.motorola.engine.graphics.Transform;
-import com.motorola.engine.graphics.Vector3;
+import com.motorola.engine.graphics.*;
 import com.motorola.engine.states.State;
+
+import java.awt.*;
+import java.util.ArrayList;
 
 public class TempestState extends State {
 
     private Renderer3D render3d;
-    private Model firstLevel;
+    private Model levelHolder;
     public GameObject levelObject;
     public GameObject player;
-
+    public Model playerModel;
+    public Camera cameraHolder;
+    public int game_try = 3;
     public TempestState(Game game) {
         super(game);
     }
@@ -25,15 +27,110 @@ public class TempestState extends State {
     public State load() {
         render3d = new Renderer3D(game);
 
+        /**
+         * First level defined
+         */
+        ArrayList<Vector3> vertices = new ArrayList<Vector3>() {
+            {
+                add(new Vector3(1, 1, 1));
+                add(new Vector3(1, 1, -1));
+                add(new Vector3(-1, 1, -1));
+                add(new Vector3(-1, 1, 1));
+
+                add(new Vector3(1, -1 , 1));
+                add(new Vector3(1, -1 , -1));
+                add(new Vector3(-1, -1, -1));
+                add(new Vector3(-1, -1, 1));
+            }
+        };
+        ArrayList<Line> edges = new ArrayList<Line>() {
+            {
+                add(new Line(0, 1, Color.BLUE));
+                add(new Line(1, 2, Color.BLUE));
+                add(new Line(2, 3, Color.BLUE));
+                add(new Line(3, 0, Color.BLUE));
+
+                add(new Line(4, 5, Color.BLUE));
+                add(new Line(5, 6, Color.BLUE));
+                add(new Line(6, 7, Color.BLUE));
+                add(new Line(7, 4, Color.BLUE));
+
+                add(new Line(0, 4, Color.BLUE));
+                add(new Line(1, 5, Color.BLUE));
+                add(new Line(2, 6, Color.BLUE));
+                add(new Line(3, 7, Color.BLUE));
+            }
+        };
+
+        //referencja do modelu levelu
+        levelHolder = new Model(vertices, edges, 200);
+
+        /**
+         * Model playera
+         */
+        ArrayList<Vector3> player_vertices = new ArrayList<Vector3>() {
+            {
+                add(new Vector3(1, 1, 1));
+                add(new Vector3(1, 1, -1));
+                add(new Vector3(-1, 1, -1));
+                add(new Vector3(-1, 1, 1));
+
+                add(new Vector3(1, -1 , 1));
+                add(new Vector3(1, -1 , -1));
+                add(new Vector3(-1, -1, -1));
+                add(new Vector3(-1, -1, 1));
+            }
+        };
+        ArrayList<Line> player_edges = new ArrayList<Line>() {
+            {
+                add(new Line(0, 1, Color.GREEN));
+                add(new Line(1, 2, Color.GREEN));
+                add(new Line(2, 3, Color.GREEN));
+                add(new Line(3, 0, Color.GREEN));
+
+                add(new Line(4, 5, Color.GREEN));
+                add(new Line(5, 6, Color.GREEN));
+                add(new Line(6, 7, Color.GREEN));
+                add(new Line(7, 4, Color.GREEN));
+
+                add(new Line(0, 4, Color.GREEN));
+                add(new Line(1, 5, Color.GREEN));
+                add(new Line(2, 6, Color.GREEN));
+                add(new Line(3, 7, Color.GREEN));
+            }
+        };
+
+        playerModel = new Model(player_vertices,player_edges,10);
+
+
+
+
+
+        /**
+         *  make level object
+         */
         levelObject = new GameObject(game,"Level");
-        levelObject.setValue("Model",firstLevel);
+        levelObject.setValue("Model",levelHolder);
         levelObject.setValue("Transform", new Transform(new Vector3(0,0,0), Quaternion.createEulerAngles(0,0,0)));
 
-        render3d.addObject(levelObject);
+        /**
+         * make player object
+         */
         player = new GameObject(game,"Player");
-        levelObject.setValue("Model", firstLevel);
-        levelObject.setValue("Transform", new Transform(new Vector3(0,0,0), Quaternion.createEulerAngles(0,0,0)));
+        player.setValue("Model", playerModel);
+        player.setValue("Transform", new Transform(new Vector3(0,0,0), Quaternion.createEulerAngles(0,0,0)));
+
+        /**
+         * Set the camera to player
+         */
+        cameraHolder = (Camera) player.setValue("Camera", new Camera(player,0.001));
+        render3d.setCamera(cameraHolder);
+
+        /**
+         * add object to renderer3d
+         */
         render3d.addObject(player);
+        render3d.addObject(levelObject);
 
         return this;
     }
